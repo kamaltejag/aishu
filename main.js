f7849dab24802337c4c271d6801edd410e39cbeb
Kamal Teja <10306@creditvidya.com>
2021-12-17 12:53:55 +0530

    Update

--- main.js
+++ main.js
@@ -4,17 +4,23 @@ function access(){
 
     if(password = "aishukamal"){
         let path = window.location.pathname;
+        console.log(path)
         path = path.split("/");
         path.pop();
+        console.log(path)
         path = path.join();
 
         let url = "";
 
+        console.log(path);
+
         if(path){
             url = window.location.protocol + "//" + window.location.host + "/" + path;
+            console.log(url)
         }
         else{
             url = window.location.protocol + "//" + window.location.host + "/" + "landing.html";
+            console.log(url)
         }
 
         window.location.href = url;
