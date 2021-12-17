891add064dbdf6815d9730e14b086de03440f70e
Kamal Teja <kamaltejag@gmail.com>
2021-12-17 13:01:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,27 +3,26 @@ function access(){
     let password = document.getElementById("password").value;
 
     if(password = "aishukamal"){
-        let path = window.location.pathname;
-        console.log(path)
-        path = path.split("/");
-        path = path.slice(0, -1);
-        console.log(path)
-        path = path.join("/");
-        console.log(path)
+//         let path = window.location.pathname;
+//         console.log(path)
+//         path = path.split("/");
+//         path = path.slice(0, -1);
+//         console.log(path)
+//         path = path.join("/");
+//         console.log(path)
 
-        let url = "";
+//         let url = "";
 
-        console.log(path);
+//         console.log(path);
 
-        if(path){
-            url = window.location.protocol + "//" + window.location.host + path;
-            console.log(url)
-        }
-        else{
-            url = window.location.protocol + "//" + window.location.host + "/" + "landing.html";
-            console.log(url)
-        }
-
-        // window.location.href = url;
+//         if(path){
+//             url = window.location.protocol + "//" + window.location.host + path;
+//             console.log(url)
+//         }
+//         else{
+//             url = window.location.protocol + "//" + window.location.host + "/" + "landing.html";
+//             console.log(url)
+//         }
+        window.location.href = "https://kamaltejag.github.io/landing.html";
     }
 }
